9454eda7913f96169900604aeb3df9b5301b5de3
wearrrrr <contact@wearr.dev>
2024-01-28 00:58:21 -0600

    Main page fully translated, add dynamic routing weeee

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
-import astroI18next from "astro-i18next";
 
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next()],
+  integrations: [],
   output: "hybrid",
   adapter: node({
     mode: "middleware",
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,19 +1,15 @@
+import en from './en.json';
+import jp from './jp.json';
+
+
 export const languages = {
     en: 'English',
     jp: "日本語",
   };
   
-  export const defaultLang = 'en';
+export const defaultLang = 'en';
   
-  export const ui = {
-    en: {
-        'nav.brand': 'Alu',
-        'nav.games': "Games",
-        'nav.settings': "Settings",
-    },
-    jp: {
-        'nav.brand': 'アルー',
-        'nav.games': "ゲーム",
-        'nav.settings': "設定",
-    },
-  } as const;
+export const ui = {
+    en,
+    jp
+};
