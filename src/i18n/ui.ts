d45cd218470ddca67af6871387682b2d4c350702
wearrrrr <contact@wearr.dev>
2024-01-28 01:02:34 -0600

    just a couple tiny changes to i18n related code.

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,12 +1,6 @@
 import en from './en.json';
 import jp from './jp.json';
 
-
-export const languages = {
-    en: 'English',
-    jp: "日本語",
-  };
-  
 export const defaultLang = 'en';
   
 export const ui = {
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,6 +1,7 @@
 import { ui, defaultLang } from './ui';
 
 export function getLangFromUrl(url: URL) {
+  // comma lol
   const [, lang] = url.pathname.split('/');
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
