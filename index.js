c8f4ceff9cbe55229180348f650a6aaab30975a2
Riftriot <slushziesrazaxc@gmail.com>
2024-03-08 09:02:19 -0600

    Simple masqr setup

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ import { build } from "astro";
 import chalk from "chalk";
 import { existsSync } from "fs";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import wisp from "wisp-server-node";
 dotenv.config();
+
+const LICENSE_SERVER_URL = "https://license.mercurywork.shop/validate?license=";
+const whiteListedDomains = ["aluu.xyz"]; // Add any public domains you have here
+const failureFile = fs.readFileSync("Checkfailed.html", "utf8");
 
 if (!existsSync("./dist")) build();
 
@@ -41,6 +46,84 @@ console.log(chalk.gray("Starting Bare..."));
 
 const app = express();
 app.use(compression({ threshold: 0, filter: () => true }));
+app.use(cookieParser());
+
+async function MasqFail(req, res) {
+  if (!req.headers.host) {
+    // no bitch still using HTTP/1.0 go away
+    return;
+  }
+  const unsafeSuffix = req.headers.host + ".html";
+  let safeSuffix = path
+    .normalize(unsafeSuffix)
+    .replace(/^(\.\.(\/|\\|$))+/, "");
+  let safeJoin = path.join(process.cwd() + "/Masqrd", safeSuffix);
+  try {
+    await fs.promises.access(safeJoin); // man do I wish this was an if-then instead of a "exception on fail"
+    const failureFileLocal = await fs.promises.readFile(safeJoin, "utf8");
+    res.setHeader("Content-Type", "text/html");
+    res.send(failureFileLocal);
+    return;
+  } catch (e) {
+    res.setHeader("Content-Type", "text/html");
+    res.send(failureFile);
+    return;
+  }
+}
+
+// Woooooo masqr yayyyy (said no one)
+// uncomment for masqr
+ /* app.use(async (req, res, next) => {
+    if (req.headers.host && whiteListedDomains.includes(req.headers.host)) {
+            next();
+            return;
+    }
+    if (req.url.includes("/bare/")) { // replace this with your bare endpoint
+        next();
+        return;
+        // Bypass for UV and other bares
+    }
+
+    const authheader = req.headers.authorization;
+    
+    if (req.cookies["authcheck"]) {
+        next();
+        return;
+    }
+
+
+    if (req.cookies['refreshcheck'] != "true") {
+        res.cookie("refreshcheck",  "true",  {maxAge: 10000}) // 10s refresh check
+        MasqFail(req, res) 
+        return;
+    }
+    
+    if (!authheader) {
+        
+        res.setHeader('WWW-Authenticate', 'Basic'); // Yeah so we need to do this to get the auth params, kinda annoying and just showing a login prompt gives it away so its behind a 10s refresh check
+        res.status(401);
+        MasqFail(req, res) 
+        return;
+    }
+
+    const auth = Buffer.from(authheader.split(' ')[1],
+        'base64').toString().split(':');
+    const user = auth[0];
+    const pass = auth[1];
+
+    const licenseCheck = ((await (await fetch(LICENSE_SERVER_URL + pass + "&host=" + req.headers.host)).json()))["status"]
+    console.log(LICENSE_SERVER_URL + pass + "&host=" + req.headers.host +" returned " +licenseCheck)
+    if (licenseCheck == "License valid") {
+        res.cookie("authcheck", "true", {expires: new Date((Date.now()) + (365*24*60*60 * 1000))}) // authorize session, for like a year, by then the link will be expired lol
+        res.send(`<script> window.location.href = window.location.href </script>`) // fun hack to make the browser refresh and remove the auth params from the URL
+        return;
+    }
+    
+    MasqFail(req, res)
+    return; 
+}) */
+
+
 app.use(express.static(path.join(process.cwd(), "static")));
 app.use(express.static(path.join(process.cwd(), "build")));
 app.use("/uv/", express.static(uvPath));
