772ad1a884fd0ecaae1cec591649397c0c4cb83a
wearrrrr <rowancole19@gmail.com>
2024-04-04 09:33:44 -0500

    Add better sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
 import { defineConfig } from "astro/config";
-
 import node from "@astrojs/node";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  site: "https://aluu.xyz",
+  integrations: [sitemap({
+    lastmod: new Date(),
+    i18n: {
+      locales: {
+        en: "en-US",
+        jp: "ja-JP",
+      },
+      defaultLocale: "en"
+    }
+  })],
   output: "hybrid",
   adapter: node({
-    mode: "middleware",
-  }),
+    mode: "middleware"
+  })
 });
